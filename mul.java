500d596ad047c37dee9c75ea026fc370f447a1c5
Tanishka Fogla <20051044@kiit.ac.in>
2022-04-26 16:38:54 +0530

    added mul.java

--- mul.java
+++ mul.java
@@ -1,8 +1,8 @@
-public class sum1{
+public class mul{
     public static void main(String[] args)
     {
         int a=5;
         int b=5;
-        System.out.println("lala"+(a+b));
+        System.out.println("lala"+(a*b));
     }
 }
